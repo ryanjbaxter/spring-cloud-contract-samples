aed64ca1a58bfdadaa99a05f950f0293f6eeb1a0
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-05-08 14:02:50 +0200

    WIP

--- consumer_with_stubs_per_consumer/src/main/java/com/example/BeerController.java
+++ consumer_with_stubs_per_consumer/src/main/java/com/example/BeerController.java
@@ -1,11 +1,8 @@
 package com.example;
 
 import java.net.MalformedURLException;
-import java.net.URI;
 
 import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,31 +27,7 @@ class BeerController {
 			value = "/beer",
 			consumes = MediaType.APPLICATION_JSON_VALUE)
 	public String gimmeABeer(@RequestBody Person person) throws MalformedURLException {
-		//remove::start[]
-		//tag::impl[]
-		ResponseEntity<Response> response = this.restTemplate.exchange(
-				RequestEntity
-						.post(URI.create("http://localhost:" + port + "/check"))
-						.contentType(MediaType.APPLICATION_JSON)
-						.body(person),
-				Response.class);
-		switch (response.getBody().status) {
-		case OK:
-			return "THERE YOU GO " + message(response.getBody());
-		default:
-			return "GET LOST " + message(response.getBody());
-		}
-		//end::impl[]
-		//remove::end[return]
-	}
-
-	String message(Response response) {
-		// if there's a name - we'll be very informal and use the name
-		if (response.name != null) {
-			return "MY DEAR FRIEND [" + response.name + "]";
-		}
-		// otherwise we'll be very official and use the surname
-		return "MR [" + response.surname + "]";
+		return null;
 	}
 }
 
@@ -73,12 +46,34 @@ class Person {
 
 class Response {
 	public ResponseStatus status;
-	// foo-service will expect this
-	public String name;
-	// bar-service will expect that
-	public String surname;
+	public String foo;
 }
 
 enum ResponseStatus {
 	OK, NOT_OK
 }
+
+
+
+/*
+
+	@RequestMapping(method = RequestMethod.POST,
+			value = "/beer",
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String gimmeABeer(@RequestBody Person person) throws MalformedURLException {
+		ResponseEntity<Response> response = this.restTemplate.exchange(
+				RequestEntity
+						.post(URI.create("http://localhost:8090/check"))
+						.contentType(MediaType.APPLICATION_JSON)
+						.body(person),
+				Response.class);
+		switch (response.getBody().status) {
+		case OK:
+			return "THERE YOU GO";
+		default:
+			return "GET LOST";
+		}
+	}
+
+
+ */
--- consumer_with_stubs_per_consumer/src/test/java/com/example/BeerControllerTest.java
+++ consumer_with_stubs_per_consumer/src/test/java/com/example/BeerControllerTest.java
@@ -1,71 +1,58 @@
 package com.example;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author Marcin Grzejszczak
  */
 @RunWith(SpringRunner.class)
-@AutoConfigureMockMvc
-@AutoConfigureJsonTesters
-//remove::start[]
-//tag::foo[]
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK,
 		properties = {"spring.application.name=foo-consumer"})
-//end::foo[]
-@AutoConfigureStubRunner(workOffline = true,
-		ids = "com.example:beer-api-producer-with-stubs-per-consumer",
-		stubsPerConsumer = true)
-//remove::end[]
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+//@AutoConfigureStubRunner(workOffline = true,
+//		ids = "com.example:beer-api-producer-with-stubs-per-consumer:+:stubs:8090",
+//		stubsPerConsumer = true)
 @DirtiesContext
 public class BeerControllerTest extends AbstractTest {
 
 	@Autowired MockMvc mockMvc;
-	@Autowired BeerController beerController;
 
-	@Value("${stubrunner.runningstubs.beer-api-producer-with-stubs-per-consumer.port}") int producerPort;
+	@Test public void should_give_me_a_beer_when_im_old_enough() throws Exception {
 
-	@Before
-	public void setupPort() {
-		beerController.port = producerPort;
 	}
 
-	//tag::impl[]
+	@Test public void should_reject_a_beer_when_im_too_young() throws Exception {
+
+	}
+}
+
+/*
+
+
 	@Test public void should_give_me_a_beer_when_im_old_enough() throws Exception {
-		//remove::start[]
 		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json.write(new Person("marcin", 22)).getJson()))
 				.andExpect(status().isOk())
-				.andExpect(content().string("THERE YOU GO MY DEAR FRIEND [marcin]"));
-		//remove::end[]
+				.andExpect(content().string("THERE YOU GO"));
 	}
 
 	@Test public void should_reject_a_beer_when_im_too_young() throws Exception {
-		//remove::start[]
 		mockMvc.perform(MockMvcRequestBuilders.post("/beer")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(json.write(new Person("marcin", 17)).getJson()))
 				.andExpect(status().isOk())
-				.andExpect(content().string("GET LOST MY DEAR FRIEND [marcin]"));
-		//remove::end[]
+				.andExpect(content().string("GET LOST"));
 	}
-	//end::impl[]
-}
+
+ */
